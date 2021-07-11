32e307f693998a2dd8e802be24287c4e2f90c950
Hao Nguyen <nguyenquochao2000@gmail.com>
2021-07-10 22:13:24 -0400

    Retrived playlists.

--- frontend/src/pages/Spotify.js
+++ frontend/src/pages/Spotify.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import axios from 'axios'
 
+// TODO: Display tracks, artists, podcasts (using components)
 class Spotify extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = {
+      playlists: [],
+    };
+    // this.search = this.search.bind(this);
   }
 
-  search() {
+  test = () => {
+    this.setState({ date: new Date() });
+    console.log(this.state.date)
+  }
+
+  search = () => {
+    // var self = this;
+    // console.log(this.state.date)
     axios.get('http://localhost:8888/playlists')
-      .then(function (response) {
+      .then((response) => {
         // handle success
         console.log(response);
+        var test = response.data.playlists.items;
+        console.log(test); 
+        var res = test.map((id) =>
+          <div>
+            <iframe src={"https://open.spotify.com/embed/playlist/"+id.id} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          </div>);
+        console.log(res);
+        this.setState({ playlists: res });
+        console.log("state playlist: " + this.state.playlists);
+
       })
       .catch(function (error) {
         // handle error
@@ -28,6 +49,9 @@ class Spotify extends React.Component {
           <button onClick={this.search}>
             Search
           </button>
+          <div>
+            {this.state.playlists}
+          </div>
         </div>
       </div>
     )
@@ -35,3 +59,5 @@ class Spotify extends React.Component {
 }
 
 export default Spotify
+
+{/* <iframe src="https://open.spotify.com/embed/track/3GtXRW9ILD3QCAH3NVdwtA" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */ }
