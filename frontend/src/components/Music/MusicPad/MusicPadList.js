9054fedc9e6508e3998d0d452902fdee9c80c6ac
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-01-02 16:05:32 -0500

    Convert class to function.

--- frontend/src/components/Music/MusicPad/MusicPadList.js
+++ frontend/src/components/Music/MusicPad/MusicPadList.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import { useState, useEffect } from 'react';
 import MusicPad from "./MusicPad";
 import './MusicPadList.css';
 
-export default class MusicPadList extends React.Component {
-  static propTypes = {
-    data: PropTypes.array,
-    title: PropTypes.string,
-    description: PropTypes.string,
-  };
+export default function MusicPadList({ data, title, description }) {
+  const [list, setList] = useState([])
 
-  render() {
-    var res = this.props.data.map((item) =>
-      <MusicPad className="list" page="dashboard" id={item.id} image={item.images[0]?.url} title={item.name} subTitle={item.description}></MusicPad>)
-    return (
-      <div>
-        <h3>{this.props.title}</h3>
-        <h5>{this.props.description}</h5>
-        <div className={"song-list-dashboard"}>
-          <span className="icon icon-circle">{'<'}</span>
-          {res}
-          <span className="icon icon-circle">{'>'}</span>
+  useEffect(() => {
+    var res = data.map((item) =>
+      <div key={item.id}>
+        <MusicPad className="list" page="dashboard" id={item.id} image={item.images[0]?.url} title={item.name} subTitle={item.description}></MusicPad>
+      </div>)
+    // setList(res)
+  })
+
+  useEffect(() => {
+    updateList();
+  }, [data])
+
+  const updateList = () => {
+    var res = data.map((item) => {
+      return (
+        <div key={item.id}>
+          <MusicPad className="list" page="dashboard" id={item.id} image={item.images[0]?.url} title={item.name} subTitle={item.description}></MusicPad>
         </div>
-      </div>
-    )
+      )
+    });
+    setList(res)
   }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h5>{description}</h5>
+      <div className="song-list-dashboard">
+        <span className="icon icon-circle">{'<'}</span>
+        {list}
+        <span className="icon icon-circle">{'>'}</span>
+      </div>
+    </div>
+  )
 }
