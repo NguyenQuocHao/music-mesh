c3000ca6f415874e397a3d0f144b574e34c0a9ad
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-01-02 13:31:22 -0500

    Make Spotify pads smaller and on row.

--- frontend/src/components/Music/MusicPad/MusicPadList.js
+++ frontend/src/components/Music/MusicPad/MusicPadList.js
@@ -3,21 +3,25 @@ import PropTypes from "prop-types";
 import MusicPad from "./MusicPad";
 import './MusicPadList.css';
 
-export default class MusicPadList extends React.Component{
+export default class MusicPadList extends React.Component {
   static propTypes = {
     data: PropTypes.array,
     title: PropTypes.string,
     description: PropTypes.string,
   };
 
-  render(){
+  render() {
     var res = this.props.data.map((item) =>
-      <a href={item.external_urls.spotify}><MusicPad className="list" id={item.id} image={item.images[0]?.url} title={item.name} subTitle={item.description}></MusicPad></a>);
-    return(
+      <MusicPad className="list" page="dashboard" id={item.id} image={item.images[0]?.url} title={item.name} subTitle={item.description}></MusicPad>)
+    return (
       <div>
         <h3>{this.props.title}</h3>
         <h5>{this.props.description}</h5>
-        {res}
+        <div className={"song-list-dashboard"}>
+          <span className="icon icon-circle">{'<'}</span>
+          {res}
+          <span className="icon icon-circle">{'>'}</span>
+        </div>
       </div>
     )
   }
