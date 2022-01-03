78049d313ee984e3b829168f2e5bc18076fc0ae8
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-01-03 03:28:34 -0500

    Add Spotify support for PlaylistPage.

--- frontend/src/components/Music/DisplayPage/PlaylistPage.js
+++ frontend/src/components/Music/DisplayPage/PlaylistPage.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useLocation, Link } from "react-router-dom"
 import SongList from '../MusicPad/SongList'
 import './PlaylistPage.css'
@@ -7,25 +7,32 @@ import './PlaylistPage.css'
 export default function PlaylistPage() {
   const { state } = useLocation();
   const [relatedVideos, setRelatedVideos] = useState([])
+  const [url, setUrl] = useState("")
 
   useEffect(() => {
-    axios.get('http://localhost:8001/relatedVideos')
-      .then(data => {
-        // console.log('Related videos: ' + JSON.stringify(data, null, 2))
-        setRelatedVideos(data.data)
-      })
+    var embededLink = ""
+    if (window.location.href.includes("youtube")) {
+      embededLink = "https://www.youtube.com/embed/" + state.id
+      axios.get('http://localhost:8001/relatedVideos')
+        .then(data => {
+          setRelatedVideos(data.data)
+        })
+    }
+    else {
+      embededLink = "https://open.spotify.com/embed/playlist/" + state.id
+    }
+
+    setUrl(embededLink)
   }, [state])
 
-  var embededLink = "https://www.youtube.com/embed/" + state.id
-  //'https://open.spotify.com/embed/playlist/'+state.id;
   return (
     <div className="background">
-    <div className="main-media">
-      <iframe width="100%" src={embededLink} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    </div>
-    <div className="song-list">      
-      <SongList data={relatedVideos} tiFtle="Related music" site="youtube" page="display"></SongList>
-    </div>
+      <div className="main-media">
+        <iframe src={url} width="100%" height="380" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen=""></iframe>
+      </div>
+      <div className="song-list">
+        <SongList data={relatedVideos} tiFtle="Related music" site="youtube" page="display"></SongList>
+      </div>
     </div>
   )
 }
