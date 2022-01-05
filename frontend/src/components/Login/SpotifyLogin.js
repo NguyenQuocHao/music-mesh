153e2b77d19840a24003837a37a9d7ea9fc89d7a
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-01-05 00:17:13 -0500

    Design login buttons. Closes #11.

--- frontend/src/components/Login/SpotifyLogin.js
+++ frontend/src/components/Login/SpotifyLogin.js
@@ -2,12 +2,12 @@ import React from 'react'
 require('dotenv').config()
 
 const AUTH_URL =
-"https://accounts.spotify.com/authorize?client_id="+process.env.REACT_APP_SPOTIFY_CLIENT_ID+"&response_type=code&redirect_uri=http://localhost:3000/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=" + process.env.REACT_APP_SPOTIFY_CLIENT_ID + "&response_type=code&redirect_uri=http://localhost:3000/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
-export default function SpotifyLogin(){
-  return(
-    <div>
-      <a href={AUTH_URL}>Login in with Spotify</a>
+export default function SpotifyLogin() {
+  return (
+    <div class="center">
+      <a href={AUTH_URL}><button class="button-1 spotify" role="button">Login in with Spotify</button></a>
     </div>
   )
 }
--- frontend/src/components/Login/YoutubeLogin.js
+++ frontend/src/components/Login/YoutubeLogin.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import "./Login.css"
+
 require('dotenv').config()
 
 const AUTH_URL =
-"https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&response_type=code&client_id="+process.env.REACT_APP_GOOGLE_CLIENT_ID+"&redirect_uri=http://localhost:3000/youtube" //http%3A%2F%2Flocalhost%3A3000%2Fyoutube
+  "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&response_type=code&client_id=" + process.env.REACT_APP_GOOGLE_CLIENT_ID + "&redirect_uri=http://localhost:3000/youtube" //http%3A%2F%2Flocalhost%3A3000%2Fyoutube
 // "https://accounts.spotify.com/authorize?client_id="+process.env.REACT_APP_CLIENT_ID+"&response_type=code&redirect_uri=http://localhost:3000/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
-export default function YoutubeLogin(){
-  return(
-    <div>
-      <a href={AUTH_URL}>Login in with Google</a>
+export default function YoutubeLogin() {
+  return (
+    <div class="center">
+      <a href={AUTH_URL}><button class="button-1 google" role="button">Login in with Google</button></a>
     </div>
   )
 }
