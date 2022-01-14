54336aecee395c7715ede6d24fc28b126a0d9c02
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-01-14 18:44:48 -0500

    Remove redundant code.

--- frontend/src/components/Dashboard/YoutubeDashboard.js
+++ frontend/src/components/Dashboard/YoutubeDashboard.js
@@ -29,7 +29,6 @@ export default function YoutubeDashboard({ }) {
 
   return (
     <div className="dashboard">
-      <iframe src="https://www.youtube.com/embed/vKhuzVQboe0" width="100%" height="380" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
       <SongList data={popularSongs} title="Popular songs" site="youtube" page="dashboard" type={vars.song}></SongList>
       <SongList data={myPlaylists} title="My Playlists" site="youtube" page="dashboard" type={vars.playlist}></SongList>
       <SongList data={randomPlaylists} title="Random playlists" site="youtube" page="dashboard" type={vars.playlist}></SongList>
