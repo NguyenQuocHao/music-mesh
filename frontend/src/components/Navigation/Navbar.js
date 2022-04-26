5ef65e751434d7f48c84636590cd25fe793b7038
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-04-26 17:39:58 -0400

    Move component to new file.

--- frontend/src/components/Navigation/Navbar.js
+++ frontend/src/components/Navigation/Navbar.js
@@ -1,25 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useLocation, Link } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
-import { SiYoutube, SiSpotify } from 'react-icons/si';
-import { AiOutlinePlus, AiOutlineLogout } from 'react-icons/ai';
+import DropdownMenu from './DropdownMenu'
 import './Navbar.scss';
-
-const unconnect = () => {
-  window.open("http://localhost:5000/auth/unconnect", "_self");
-};
-
-const logout = () => {
-  window.open("http://localhost:5000/auth/logout", "_self");
-};
-
-const goToSpotify = () => {
-  window.open("http://localhost:3000/spotify", "_self");
-};
-
-const goToYoutube = () => {
-  window.open("http://localhost:3000/youtube", "_self");
-};
 
 function Navbar({ user, sideBarHandler }) {
   const { state } = useLocation();
@@ -59,97 +41,6 @@ function NavItem(props) {
 
       {open && props.children}
     </li>
-  );
-}
-
-function DropdownMenu({ mainAccount }) {
-  const [activeMenu, setActiveMenu] = useState('main');
-  const [menuHeight, setMenuHeight] = useState(null);
-  const dropdownRef = useRef(null);
-
-  useEffect(() => {
-    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight + 30)
-  }, [])
-
-  function calcHeight(el) {
-    const height = el.offsetHeight;
-    setMenuHeight(height + 30);
-  }
-
-  function DropdownItem(props) {
-    function goToMenu() {
-      if (props.goToMenu) {
-        setActiveMenu(props.goToMenu);
-      }
-    }
-    
-    return (
-      <a href="#" className="menu-item" onClick={props.handler ? props.handler : goToMenu}>
-        {props.leftIcon ? <span className="icon-button">{props.leftIcon}</span> : null}
-        {props.children}
-        <span className="icon-right">{props.rightIcon}</span>
-      </a>
-    );
-  }
-
-  return (
-    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
-      <CSSTransition
-        in={activeMenu === 'main'}
-        timeout={500}
-        classNames="menu-primary"
-        unmountOnExit
-        onEnter={calcHeight}
-        >
-        <div className="menu">
-          <DropdownItem
-            leftIcon={<img
-              src={mainAccount.photos[0]?.value}
-              alt=""
-              className="avatar"
-            />}
-            rightIcon={mainAccount.provider === "google" ? <SiYoutube /> : <SiSpotify />}>
-            {mainAccount.displayName}
-          </DropdownItem>
-          {
-            mainAccount.linkedAccount ?
-              <DropdownItem
-                leftIcon={<img
-                  src={mainAccount.linkedAccount.photos[0]?.value}
-                  alt=""
-                  className="avatar"
-                />}
-                rightIcon={mainAccount.linkedAccount.provider === "google" ? <SiYoutube /> : <SiSpotify />}
-                goToMenu="unlink">
-                {mainAccount.linkedAccount.displayName}
-              </DropdownItem>
-              :
-              <DropdownItem leftIcon={<AiOutlinePlus />} handler={mainAccount.provider === "google" ? goToSpotify : goToYoutube}>
-                Connect account
-              </DropdownItem>
-          }
-          <DropdownItem leftIcon={<AiOutlineLogout />} handler={logout}>
-            Logout
-          </DropdownItem>
-        </div>
-      </CSSTransition>
-
-      <CSSTransition
-        in={activeMenu === 'unlink'}
-        timeout={500}
-        classNames="menu-secondary"
-        unmountOnExit
-        onEnter={calcHeight}
-        >
-        <div className="menu">
-          <DropdownItem>
-            <h3>Do you want to unlink this account?</h3>
-          </DropdownItem>
-          <DropdownItem handler={unconnect}>Yes</DropdownItem>
-          <DropdownItem goToMenu="main">No</DropdownItem>
-        </div>
-      </CSSTransition>
-    </div>
   );
 }
 
