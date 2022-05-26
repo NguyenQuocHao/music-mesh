81f828abd68c0b11434f1ad3a0846eb8b5718de6
NguyenQuocHao <nguyenquochao2000@gmail.com>
2022-05-26 02:11:31 -0400

    Remove redundant slash.

--- client/src/variables.js
+++ client/src/variables.js
@@ -2,8 +2,8 @@ const youtube = "youtube"
 const spotify = "spotify"
 const playlist = "playlist"
 const song = "song"
-export const HOST = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://mus1c-mesh.herokuapp.com/"
-export const CLIENT = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mus1c-mesh.herokuapp.com/"
+export const HOST = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://mus1c-mesh.herokuapp.com"
+export const CLIENT = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mus1c-mesh.herokuapp.com"
 
 export default {
     youtube, spotify, playlist, song, HOST
--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
-const HOST = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://mus1c-mesh.herokuapp.com/"
-const CLIENT = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mus1c-mesh.herokuapp.com/"
+const HOST = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://mus1c-mesh.herokuapp.com"
+const CLIENT = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mus1c-mesh.herokuapp.com"
 
 exports.HOST = HOST
 exports.CLIENT = CLIENT
